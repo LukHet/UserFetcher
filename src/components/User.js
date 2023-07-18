e2ff9ac58e3beb90e9383f124926b2fe35376dc9
LukHet <lukaszhetman1@gmail.com>
2023-07-19 01:55:06 +0200

    Deleted unnecessary files and added additional user info and component styling.

--- src/components/User.js
+++ src/components/User.js
@@ -2,8 +2,32 @@ import "../styles/User.css";
 
 const User = (props) => {
   return (
-    <div className="User">
-      {props.firstname} {props.lastname}
+    <div>
+      <div className="user">
+        <div className="username">
+          {props.firstname} {props.lastname}
+        </div>
+        <div className="userinfo">
+          <div>
+            <div>Birth date: {props.birthdate}</div>
+            <div>City: {props.city}</div>
+            <div>Address: {props.address}</div>
+          </div>
+          <div className="userbodyinfo">
+            Measurements:
+            <div>Gender: {props.gender}</div>
+            <div>Weight: {props.weight}</div>
+            <div>Height: {props.height}</div>
+          </div>
+        </div>
+        <img src={props.image} alt="user" className="userimg" />
+        <div className="usercontact">
+          Contact:
+          <div>E-mail: {props.email}</div>
+          <div>Phone number: {props.phone}</div>
+        </div>
+      </div>
+      <div className="separation" />
     </div>
   );
 };
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,7 +3,20 @@ import "../styles/UsersList.css";
 
 const UsersList = (props) => {
   const usersList = props.users?.map((user) => (
-    <User key={user.id} firstname={user.firstName} lastname={user.lastName} />
+    <User
+      key={user.id}
+      firstname={user.firstName}
+      lastname={user.lastName}
+      image={user.image}
+      birthdate={user.birthDate}
+      email={user.email}
+      gender={user.gender}
+      phone={user.phone}
+      weight={user.weight}
+      height={user.height}
+      address={user.address.address}
+      city={user.address.city}
+    />
   ));
   return <div className="UsersList">{usersList}</div>;
 };
